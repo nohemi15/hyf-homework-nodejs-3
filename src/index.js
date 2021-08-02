7fe525f2741644935e185febafc1130e16208f03
nohemi15 <86972966+nohemi15@users.noreply.github.com>
2021-08-02 13:20:58 -0600

    POST users

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
 const express = require('express');
 const app = express();
 
-const mockUserData = [];
+const mockUsername = [];
 
-app.get('/', function(req, res) {
+
+
+app.get('/', function(req,res){
 
   res.send('Hello World!');
 
 });
 
-app.get('/users', function(req, res){
+app.get('/users', function(req,res){
+  const usr = mockUsername.map((item)=>({id:item}));
+    res.json(usr);
+});
 
-	res.send(mockUserData);
+app.post('/user',(req,res)=>{
+
+  mockUsername.push(mockUsername.length);
+  const user = {id:mockUsername[mockUsername.length-1]}; 
+  res.status(200).json(user);  
 
 });
 
-app.listen(3000, function(){console.log('Server is listening')})
+app.get('/user/:id', (req,res)=>{
 
+    const index = mockUsername.indexOf(Number(req.params.id));
+    
+    console.log(index)
+
+    if (index < 0){
+        return res.sendStatus(204);
+        
+    }
+    return res.json({id:Number(req.params.id)});
+    
+});
+
+app.listen(3000, function(){console.log('server is listening')})
